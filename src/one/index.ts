78b53d789bac2230aaca53de4150e8e0b763126f
Aurora <aurora@4d2.org>
2026-01-28 16:14:17 +0100

    add `@` path alias

--- src/one/index.ts
+++ src/one/index.ts
@@ -1,6 +1,6 @@
 import { computed, MaybeRefOrGetter, Ref, toValue } from 'vue'
-import { min } from '../min'
-import { max } from '../max'
+import { min } from '@/min'
+import { max } from '@/max'
 
 export function one(...values: MaybeRefOrGetter<boolean>[]): Readonly<Ref<boolean>> {
 	const mn = min(1, ...values), mx = max(1, ...values)

2529c6fe7c99e6079df8d7894f986c5cf43e6039
Aurora <aurora@4d2.org>
2026-01-31 17:22:33 +0100

    change `one` implementation to use `exact`

--- src/one/index.ts
+++ src/one/index.ts
@@ -14,14 +14,11 @@
  * limitations under the License.
  */
 
-import { computed, MaybeRefOrGetter, Ref } from 'vue'
-import { min } from '@/min'
-import { max } from '@/max'
+import { MaybeRefOrGetter, Ref } from 'vue'
+import { exact } from '@/exact'
 
 export function one(
-	...values: MaybeRefOrGetter<boolean>[]
+	...sources: MaybeRefOrGetter<boolean>[]
 ): Readonly<Ref<boolean>> {
-	const mn = min(1, ...values),
-		mx = max(1, ...values)
-	return computed(() => mx.value && mn.value)
+	return exact(1, ...sources)
 }
